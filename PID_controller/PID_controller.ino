c0b0b5c20e56aa6787f073b85b69133da8392909
ohm <vaghelaohm@gmail.com>
2023-01-18 13:00:16 +0530

    PID-Controller remaining + few data type updates in other two dir

--- PID_controller/PID_controller.ino
+++ PID_controller/PID_controller.ino
@@ -1,3 +1,20 @@
+///////////////////////////////////////////////////
+//BLDC
+#include <SD.h>
+#include "IFX007T-Motor-Control.h"
+#define WEAKENING  0           // 0=normal, 1=weak (fast)
+const float stepsPerDegree = 10.0; // Adjust this value based on your motor
+String input = "+";
+int direction = 1;
+//float angle[6] = {1, 50, 200, 300, 600, 800} ;
+float rpm=0;
+uint8_t dutyCycle = 30;
+//Create an instance of 'IFX007TMotorControl' called 'MyMotor'
+IFX007TMotorControl MyMotor = IFX007TMotorControl();
+///////////////////////////////////////////////////
+
+///////////////////////////////////////////////////
+//Encoder
 bool Aold = false;
 bool Bold = false;
 bool Anew = false;
@@ -11,14 +28,25 @@ const byte ChB = 20;
 
 int GearReduction = 1;
 float PPR = 16384*GearReduction;
+///////////////////////////////////////////////////
 
 void setup() {
   Serial.begin(115200);
+  Serial.println(" Infineon hall sensor BLDC motor test! ");
 
   pinMode(ChA, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(ChA), changeA, CHANGE);
   pinMode(ChB, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(ChB), changeB, CHANGE);
+
+  MyMotor.begin();
+  // Adapt the following values according to the README if necessary
+  MyMotor.MotorParam.MotorPolepairs =11;        // Amount of polepairs. If your motor has 8 poles, it has 4 pole PAIRS
+  MyMotor.MotorParam.SensingMode = 1;           // If you use a Hallsensor set 1, for sensorless application 0
+  MyMotor.MotorParam.PI_Reg_P = 0.01;           // P value for the PI-RPM regulator
+  MyMotor.MotorParam.PI_Reg_I = 0.01;          // I value for the PI-RPM regulator
+  MyMotor.configureBLDCMotor(MyMotor.MotorParam);
+
 
 }
 
@@ -26,7 +54,15 @@ void loop() {
 
   Serial.print("Angle : ");
   Serial.println(angle);
+  
   updateAngle();
+}
+
+void PIDControl()
+{
+    // maxDutyCycle :50
+    // minDutyCycle :30
+    
 }
 
 void updateAngle()
